8c68e13034704955d33a032db14981144626a110
Saglara <nohaevass@yandex.ru>
2023-06-27 11:21:13 +0300

    Update dz1.java

--- dz1.java
+++ dz1.java
@@ -4,5 +4,18 @@
 
 
 public class dz1 {
-    
+    public static int[] main(String[] args) {
+
+        if (arr1.length != arr2.length) {
+            System.out.println("Введите массивы равной длины: ");
+        }
+
+        public static int[] newArr ( int[] arr1, int[] arr2){
+            int[] arr3 = new int[arr1.length];
+            for (int i = 0; i < arr1.length; i++) {
+                arr3[i] = arr1[i] - arr2[i];
+            }
+            return arr3;
+        }
+    }
 }
